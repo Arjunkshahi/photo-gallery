2ead50cac13f93fd5ff566ed7a58b6b18b265608
Arjunkshahi <135677689+Arjunkshahi@users.noreply.github.com>
2024-12-10 14:21:17 -0500

    reverted to previous storage backend

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,15 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'photogallery',
+  name: 'amplifyTeamDrive',
   access: (allow) => ({
-    'media/*': [
-      allow.guest.to(['read']) // additional actions such as "write" and "delete" can be specified depending on your use case
-    ]
+    'profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
   })
 });
